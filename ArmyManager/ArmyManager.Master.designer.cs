de0c274d0e30fd952bca32c0d9841a8b121190dc
elispb <elis.p.brown@gmail.com>
2019-12-09 20:25:00 +0100

    Add homepage to master

--- ArmyManager/ArmyManager.Master.designer.cs
+++ ArmyManager/ArmyManager.Master.designer.cs
@@ -31,6 +31,15 @@ namespace ArmyManager {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Home control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink Home;
+        
+        /// <summary>
         /// AddUnitLink control.
         /// </summary>
         /// <remarks>
